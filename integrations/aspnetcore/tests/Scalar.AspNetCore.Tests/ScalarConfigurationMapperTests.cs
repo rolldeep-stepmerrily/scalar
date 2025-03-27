e26ef855022b140c1a8dbb01742cf069e6f1638d
Justin Lampe <xC0dex@users.noreply.github.com>
2025-03-27 15:53:31 +0100

    Scalar.Aspire integration

--- integrations/aspnetcore/tests/Scalar.AspNetCore.Tests/ScalarConfigurationMapperTests.cs
+++ integrations/aspnetcore/tests/Scalar.AspNetCore.Tests/ScalarConfigurationMapperTests.cs
@@ -1,6 +1,6 @@
 namespace Scalar.AspNetCore.Tests;
 
-public class ScalarOptionsMapperTests
+public class ScalarConfigurationMapperTests
 {
     [Fact]
     public void ToConfiguration_ShouldReturnCorrectDefaultConfiguration()
@@ -149,7 +149,7 @@ public class ScalarOptionsMapperTests
         var hiddenClients = (IDictionary<string, IEnumerable<string>>) options.ToScalarConfiguration().HiddenClients!;
 
         // Assert
-        hiddenClients.Should().HaveCount(ScalarOptionsMapper.ClientOptions.Count - 1);
+        hiddenClients.Should().HaveCount(ScalarConfigurationMapper.ClientOptions.Count - 1);
         hiddenClients.Should().NotContainKey("csharp");
     }
 
@@ -163,7 +163,7 @@ public class ScalarOptionsMapperTests
         var hiddenClients = (IDictionary<string, IEnumerable<string>>) options.ToScalarConfiguration().HiddenClients!;
 
         // Assert
-        hiddenClients.Should().HaveCount(ScalarOptionsMapper.ClientOptions.Count);
+        hiddenClients.Should().HaveCount(ScalarConfigurationMapper.ClientOptions.Count);
         hiddenClients.Should().ContainKey("csharp")
             .WhoseValue.Should().ContainSingle().Which.Should().Be("restsharp");
         hiddenClients.Should().ContainKey("python")
@@ -180,7 +180,7 @@ public class ScalarOptionsMapperTests
         var hiddenClients = (IDictionary<string, IEnumerable<string>>) options.ToScalarConfiguration().HiddenClients!;
 
         // Assert
-        hiddenClients.Should().HaveCount(ScalarOptionsMapper.ClientOptions.Count - 1);
+        hiddenClients.Should().HaveCount(ScalarConfigurationMapper.ClientOptions.Count - 1);
         hiddenClients.Should().NotContainKey("kotlin");
     }
 
